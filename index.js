231ce59ead82c7c9a5f45903fba95083f0433e21
Alamin4D <alaminahmed79000@gmail.com>
2024-06-08 11:45:00 +0600

    users api added

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const express = require('express')
 const app = express()
 require('dotenv').config()
 const cors = require('cors')
-// const cookieParser = require('cookie-parser')
-const { MongoClient, ServerApiVersion } = require('mongodb')
-// const jwt = require('jsonwebtoken')
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb')
+const jwt = require('jsonwebtoken')
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
 
 const port = process.env.PORT || 5000
 
@@ -17,26 +17,24 @@ const corsOptions = {
 app.use(cors(corsOptions))
 
 app.use(express.json())
-// app.use(cookieParser())
 
 // Verify Token Middleware
-// const verifyToken = async (req, res, next) => {
-//   const token = req.cookies?.token
-//   console.log(token)
-//   if (!token) {
-//     return res.status(401).send({ message: 'unauthorized access' })
-//   }
-//   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-//     if (err) {
-//       console.log(err)
-//       return res.status(401).send({ message: 'unauthorized access' })
-//     }
-//     req.user = decoded
-//     next()
-//   })
-// }
+const verifyToken = (req, res, next) => {
+  console.log('inside verify token', req.headers.authorization);
+  if (!req.headers.authorization) {
+      return res.status(401).send({ message: 'forbidden access' })
+  }
+  const token = req.headers.authorization.split(' ')[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+      if (err) {
+          return res.status(401).send({ message: 'forbidden access' })
+      }
+      req.decoded = decoded;
+      next();
+  })
+}
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@main.mq0mae1.mongodb.net/?retryWrites=true&w=majority&appName=Main`
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.5rmxtse.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -48,53 +46,82 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     const scholarshipsCollection = client.db('battleDB').collection('scholarships')
+    // const usersCollection = client.db('battleDB').collection('users')
     // auth related api
-    // app.post('/jwt', async (req, res) => {
-    //   const user = req.body
-    //   const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
-    //     expiresIn: '365d',
-    //   })
-    //   res
-    //     .cookie('token', token, {
-    //       httpOnly: true,
-    //       secure: process.env.NODE_ENV === 'production',
-    //       sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
-    //     })
-    //     .send({ success: true })
-    // })
-    // Logout
-    // app.get('/logout', async (req, res) => {
-    //   try {
-    //     res
-    //       .clearCookie('token', {
-    //         maxAge: 0,
-    //         secure: process.env.NODE_ENV === 'production',
-    //         sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
-    //       })
-    //       .send({ success: true })
-    //     console.log('Logout successful')
-    //   } catch (err) {
-    //     res.status(500).send(err)
-    //   }
-    // })
+    app.post('/jwt', async (req, res) => {
+      const user = req.body
+      console.log(user)
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+      res.send({ token })
+    })
 
-    // Get all rooms from db
-    // app.get('/rooms', async (req, res) => {
-    //   const category = req.query.category
-    //   console.log(category)
-    //   let query = {}
-    //   if (category && category !== 'null') query = { category }
-    //   const result = await roomsCollection.find(query).toArray()
+    // create-payment-intent
+    app.post('/create-payment-intent', async (req, res) => {
+      const price = req.body.price
+      const priceInCent = parseFloat(price) * 100
+      if (!price || priceInCent < 1) return
+      // generate clientSecret
+      const { client_secret } = await stripe.paymentIntents.create({
+        amount: priceInCent,
+        currency: 'usd',
+        // In the latest version of the API, specifying the `automatic_payment_methods` parameter is optional because Stripe enables its functionality by default.
+        automatic_payment_methods: {
+          enabled: true,
+        },
+      })
+      // send client secret as response
+      res.send({ clientSecret: client_secret })
+    })
+
+    // get a user info by email from db
+    //  app.get('/user/:email', async (req, res) => {
+    //   const email = req.params.email
+    //   const result = await usersCollection.findOne({ email })
     //   res.send(result)
     // })
 
-    // Get a single room data from db using _id
-    // app.get('/room/:id', async (req, res) => {
-    //   const id = req.params.id
-    //   const query = { _id: new ObjectId(id) }
-    //   const result = await roomsCollection.findOne(query)
-    //   res.send(result)
-    // })
+    app.get('/scholarships', async (req, res) => {
+      const result = await scholarshipsCollection.find().toArray()
+      res.send(result)
+    })
+
+    app.get('/scholarship/:id', async (req, res) => {
+      const id = req.params.id
+      const query = { _id: new ObjectId(id) }
+      const result = await scholarshipsCollection.findOne(query)
+      res.send(result)
+    })
+
+    // Get all scholarship data from db for pagination
+    app.get('/scholarship', async (req, res) => {
+      const size = parseInt(req.query.size) || 8
+      const page = parseInt(req.query.page) - 1
+      const search = req.query.search || ''
+      console.log(size, page)
+
+      let query = {
+        name: { $regex: search, $options: 'i' },
+      }
+      let options = {}
+      const result = await scholarshipsCollection
+        .find(query, options)
+        .skip(page * size)
+        .limit(size)
+        .toArray()
+
+      res.send(result)
+    })
+
+    // Get all jobs data count from db
+    app.get('/jobs-count', async (req, res) => {
+      const search = req.query.search
+      let query = {
+        name: { $regex: search, $options: 'i' },
+      }
+      const count = await scholarshipsCollection.countDocuments(query)
+
+      res.send({ count })
+    })
 
     // Send a ping to confirm a successful connection
     await client.db('admin').command({ ping: 1 })
@@ -108,9 +135,9 @@ async function run() {
 run().catch(console.dir)
 
 app.get('/', (req, res) => {
-    res.send('My Battle server is running')
+  res.send('My Battle server is running')
 })
 
 app.listen(port, () => {
-    console.log(`My server is running on port: ${port}`)
+  console.log(`My server is running on port: ${port}`)
 })
