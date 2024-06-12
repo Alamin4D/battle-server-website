51c5a25492c080baee9ea322dbda78bd1b524bdd
Alamin4D <alaminahmed79000@gmail.com>
2024-06-12 23:57:03 +0600

    finished the project

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 5000
 
 // middleware
 const corsOptions = {
-  origin: ['http://localhost:5173', 'http://localhost:5174'],
+  origin: ['http://localhost:5173', 'http://localhost:5174', 'https://6669d89c3bf1500422f2b447--scintillating-rabanadas-b8327e.netlify.app'],
   credentials: true,
   optionSuccessStatus: 200,
 }
@@ -22,15 +22,15 @@ app.use(express.json())
 const verifyToken = (req, res, next) => {
   console.log('inside verify token', req.headers.authorization);
   if (!req.headers.authorization) {
-      return res.status(401).send({ message: 'forbidden access' })
+    return res.status(401).send({ message: 'forbidden access' })
   }
   const token = req.headers.authorization.split(' ')[1];
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-      if (err) {
-          return res.status(401).send({ message: 'forbidden access' })
-      }
-      req.decoded = decoded;
-      next();
+    if (err) {
+      return res.status(401).send({ message: 'forbidden access2' })
+    }
+    req.decoded = decoded;
+    next();
   })
 }
 
@@ -46,7 +46,23 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     const scholarshipsCollection = client.db('battleDB').collection('scholarships')
-    // const usersCollection = client.db('battleDB').collection('users')
+    const usersCollection = client.db('battleDB').collection('users')
+    const appliedCollection = client.db('battleDB').collection('applied')
+    const reviewsCollection = client.db('battleDB').collection('reviews')
+
+     // verify admin middleware
+     const verifyAdmin = async (req, res, next) => {
+      console.log('hello')
+      const user = req.decoded
+      const query = { email: user?.email }
+      console.log(query)
+      const result = await usersCollection.findOne(query)
+      console.log(result?.role)
+      if (!result || result?.role !== 'admin')
+        return res.status(401).send({ message: 'unauthorized access!!' })
+
+      next()
+    }
     // auth related api
     app.post('/jwt', async (req, res) => {
       const user = req.body
@@ -73,15 +89,142 @@ async function run() {
       res.send({ clientSecret: client_secret })
     })
 
+    // save a user data in db
+    app.put('/user', async (req, res) => {
+      const user = req.body
+      const query = { email: user?.email }
+      // check if user already exists in db
+      const isExist = await usersCollection.findOne(query)
+      if (isExist) {
+        if (user.status === 'Requested') {
+          // if existing user try to change his role
+          const result = await usersCollection.updateOne(query, {
+            $set: { status: user?.status },
+          })
+          return res.send(result)
+        } else {
+          // if existing user login again
+          return res.send(isExist)
+        }
+      }
+
+      // save user for the first time
+      const options = { upsert: true }
+      const updateDoc = {
+        $set: {
+          ...user,
+        },
+      }
+      const result = await usersCollection.updateOne(query, updateDoc, options)
+      res.send(result)
+    })
+
     // get a user info by email from db
-    //  app.get('/user/:email', async (req, res) => {
-    //   const email = req.params.email
-    //   const result = await usersCollection.findOne({ email })
-    //   res.send(result)
-    // })
+    app.get('/user/:email', async (req, res) => {
+      const email = req.params.email
+      const result = await usersCollection.findOne({ email })
+      res.send(result)
+    })
+
+    // users related api
+    app.get('/users', verifyToken, verifyAdmin, async (req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+  })
+
+  //update a user role
+  app.patch('/users/update/:email', async (req, res) => {
+    const email = req.params.email
+    const user = req.body
+    const query = { email }
+    const updateDoc = {
+      $set: { ...user, timestamp: Date.now() },
+    }
+    const result = await usersCollection.updateOne(query, updateDoc)
+    res.send(result)
+  })
+
+
+  app.delete('/users/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await usersCollection.deleteOne(query);
+      res.send(result);
+  })
 
     app.get('/scholarships', async (req, res) => {
       const result = await scholarshipsCollection.find().toArray()
+      res.send(result)
+    })
+
+    // Save a scholarship data in db
+    app.post('/add-scholarship', async (req, res) => {
+      const scholarshipData = req.body
+      const result = await scholarshipsCollection.insertOne(scholarshipData)
+      res.send(result)
+    })
+
+    // update scholarship data
+    app.put('/scholarship/update/:id', verifyToken, async (req, res) => {
+      const id = req.params.id
+      const scholarshipData = req.body
+      const query = { _id: new ObjectId(id) }
+      const updateDoc = {
+        $set: scholarshipData,
+      }
+      const result = await scholarshipsCollection.updateOne(query, updateDoc)
+      res.send(result)
+    })
+
+    // get all applier for moderator
+    app.get('/all-applied/:email', async (req, res) => {
+      const email = req.params.email
+
+      let query = { 'userData.email': email }
+      console.log(query)
+      const result = await appliedCollection.find(query).toArray()
+      res.send(result)
+    })
+
+    // delete a apply scholarship
+    app.delete('/all-applied/:id', async (req, res) => {
+      const id = req.params.id
+      const query = { _id: new ObjectId(id) }
+      const result = await scholarshipsCollection.deleteOne(query)
+      res.send(result)
+    })
+
+    app.get('/reviews', async (req, res) => {
+      const result = await reviewsCollection.find().toArray()
+      res.send(result)
+    })
+
+    // get all applier for moderator
+    app.get('/all-review/:email', async (req, res) => {
+      const email = req.params.email
+
+      let query = { 'userData.email': email }
+      const result = await reviewsCollection.find(query).toArray()
+      res.send(result)
+    })
+
+    // update scholarship data
+    app.put('/review/update/:id', verifyToken, async (req, res) => {
+      const id = req.params.id
+      const reviewData = req.body
+      const query = { _id: new ObjectId(id) }
+      const updateDoc = {
+        $set: reviewData,
+      }
+      const result = await reviewsCollection.updateOne(query, updateDoc)
+      res.send(result)
+    })
+
+    // delete a review
+    app.delete('/all-review/:id', async (req, res) => {
+      const id = req.params.id
+      const query = { _id: new ObjectId(id) }
+      const result = await reviewsCollection.deleteOne(query)
       res.send(result)
     })
 
@@ -89,6 +232,25 @@ async function run() {
       const id = req.params.id
       const query = { _id: new ObjectId(id) }
       const result = await scholarshipsCollection.findOne(query)
+      res.send(result)
+    })
+
+    app.get('/applied', async (req, res) => {
+      const result = await appliedCollection.find().toArray()
+      res.send(result)
+    })
+
+     // Save a booking data in db
+     app.post('/applied-scholarship', verifyToken, async (req, res) => {
+      const appliedData = req.body
+      const result = await appliedCollection.insertOne(appliedData)
+      res.send(result)
+    })
+
+     // Save a review data in db
+     app.post('/add-review', verifyToken, async (req, res) => {
+      const reviewData = req.body
+      const result = await reviewsCollection.insertOne(reviewData)
       res.send(result)
     })
 
